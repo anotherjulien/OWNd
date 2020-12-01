9da73f6476f6596ee225caad417d4330cc739a8a
Julien <julien@vinter.fr>
2020-12-01 21:01:39 +0100

    v0.7.12 - SOAP exception handling

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="OWNd",
-    version="0.7.11",
+    version="0.7.12",
     author="anotherjulien",
     url="https://github.com/anotherjulien/OWNd",
     author_email="yetanotherjulien@gmail.com",
