1084faaa1442dd7cd73d6922fd7def7ea26b1e6e
anotherjulien <yetanotherjulien@gmail.com>
2021-01-12 23:25:42 +0100

    Fixed zone handling in heating commands

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="OWNd",
-    version="0.7.19",
+    version="0.7.20",
     author="anotherjulien",
     url="https://github.com/anotherjulien/OWNd",
     author_email="yetanotherjulien@gmail.com",
