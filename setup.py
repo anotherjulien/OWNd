a1008fef8a8ec35a485a130c428e9be2131553bb
anotherjulien <yetanotherjulien@gmail.com>
2021-03-18 02:10:52 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="OWNd",
-    version="0.7.23",
+    version="0.7.24",
     author="anotherjulien",
     url="https://github.com/anotherjulien/OWNd",
     author_email="yetanotherjulien@gmail.com",
